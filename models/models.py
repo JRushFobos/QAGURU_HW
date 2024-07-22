d5aab58bab9ae4d7852a3e72d527f8e5b412d351
Evgeniy Mokrushin <fobos_media@mail.ru>
2024-07-23 02:26:22 +0500

    refactoring

--- models/models.py
+++ models/models.py
@@ -1,9 +1,41 @@
 from pydantic import BaseModel
+from typing import Optional
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
+
+engine = create_engine("sqlite:///./users.db", echo=True)
+
+Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
 
-class UserData(BaseModel):
-    id: int
-    email: str
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True, index=True)
+    avatar = Column(String)
+
+
+class UserCreate(BaseModel):
     first_name: str
     last_name: str
+    email: str
     avatar: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    avatar: str
+
+
+class UserUpdate(BaseModel):
+    email: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    avatar: Optional[str] = None
