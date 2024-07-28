e10a4813dbd24581c89f338038f9e55554274eaa
Evgeniy Mokrushin <fobos_media@mail.ru>
2024-07-28 23:40:27 +0500

    Chenge after review №1

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import os
 
 import dotenv
 import pytest
+import requests
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -12,3 +13,11 @@ def envs():
 @pytest.fixture(scope="session")
 def app_url():
     return os.getenv("APP_URL")
+
+
+@pytest.fixture(scope="session")
+def check_empty_database(app_url):
+    response = requests.get(f"{app_url}/api/users")
+    count_users = response.json()["total"]
+    if count_users == 0:
+        pytest.skip(f"Database is not empty, {count_users} users found.")
--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
 import json
+
+import pytest
 import requests
 from random import randint
 
 from jsonschema import validate
 from faker import Faker
 
+from models.models import PaginatedResponse
 from schemas.schemas import valid_schema_all_users, valid_schema_one_user
 
 fake = Faker()
@@ -125,9 +128,7 @@ def test_dell_one_user_status(app_url):
 # Tecты пагинации
 def test_attrs_pagination(app_url):
     response = requests.get(f"{app_url}/api/users")
-    pagination_attrs = ["total", "page", "size", "pages"]
-    for attr in pagination_attrs:
-        assert attr in response.json(), f"Attribute {attr} is missing "
+    PaginatedResponse(**response.json())
 
 
 # ожидаемое количество объектов в ответе;
@@ -140,29 +141,19 @@ def test_objs_in_resp_pagination(app_url):
 
 
 # правильное количество страниц при разных значениях size;
-def test_count_pages_by_size_pagination(app_url):
+@pytest.mark.parametrize("size", [1, 5, 10, 25])
+def test_count_pages_by_size_pagination(app_url, size, check_empty_database):
     response = requests.get(f"{app_url}/api/users")
-    pagination_data = response.json()
-    count_users = pagination_data["total"]
-    assert count_users > 0, "BD is empty"
-
-    for size in [1, 5, 10, 25, count_users]:
-        response = requests.get(f"{app_url}/api/users/?size={size}")
-        assert response.status_code == 200, "Error with pagination"
-
-        pagination_data = response.json()
-        expected_pages = (count_users + size - 1) // size
-
-        # Проверяем, что количество страниц соответствует ожиданиям
-        assert pagination_data["pages"] == expected_pages, \
-            f"{expected_pages} pages were expected, but {pagination_data['pages']} as received"
-
-        # Дополнительная проверка: количество элементов на странице не должно превышать size
-        items_count = len(pagination_data["items"])
-        assert items_count <= size, "The number of elements on the page must not exceed the page size"
+    count_users = response.json()["total"]
+    response = requests.get(f"{app_url}/api/users/?size={size}")
+    expected_pages = (count_users + size - 1) // size
+    assert response.json()["pages"] == expected_pages, \
+        f"{expected_pages} pages were expected, but {response.json()['pages']} as received"
+    items_count = len(response.json()["items"])
+    assert items_count <= size, "The number of elements on the page must not exceed the page size"
 
 
-# возвращаются разные данные при разных значениях page;
+# возвращаются разные данные при разных значениях size;
 def test_resp_by_size_pagination(app_url):
     response = requests.get(f"{app_url}/api/users")
     count = len(response.json()["items"])
@@ -172,6 +163,19 @@ def test_resp_by_size_pagination(app_url):
     response = requests.get(f"{app_url}/api/users/?size={random_size-1}")
     response_size_2 = response.json()["items"]
     assert (response_size_1 != response_size_2), "Response the same"
+
+
+# возвращаются разные данные при разных значениях page;
+def test_diff_resp_by_page(app_url):
+    response = requests.get(f"{app_url}/api/users")
+    count = len(response.json()["items"])
+    random_page = randint(2, count)
+    random_size = randint(1, count)
+    response = requests.get(f"{app_url}/api/users/?size={random_size}&page={random_page}")
+    response_page_1 = response.json()["items"]
+    response = requests.get(f"{app_url}/api/users/?size={random_size}&page={random_page-1}")
+    response_page_2 = response.json()["items"]
+    assert (response_page_1 != response_page_2), "Response the same"
 
 
 # Негативные проверки
